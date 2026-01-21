bd0de40b8930c9fcb46a28c740c66985dc351349
Manish <manish6820058@gmail.com>
2026-01-21 21:21:17 +0530

    Fix MySQL cloud connection

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createPool({
+const db = sql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
